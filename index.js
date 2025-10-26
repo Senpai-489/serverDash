473f2113f4280650a8faf14f04731cb8bc69469f
Amit Singh <amit489@gmail.com>
2025-10-26 18:18:19 +0530

    sheet limit Increase

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use("/api/", authRoutes);
 app.use("/api/", dataRoutes);
